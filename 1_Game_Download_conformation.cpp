77adb76c4c4401f7a4773416615999a746f7a9df
Saad Shafique <saadshafiquee@gmail.com>
2025-11-01 19:29:43 +0500

    Add files via upload

---
+++ 1_Game_Download_conformation.cpp
@@ -0,0 +1,29 @@
+// //Small Game Entry confirmation
+
+
+
+// #include <iostream> //This includes the functionality of input and output
+// #include <string> // This add string functionality
+// using namespace std; // the solves the naming comflict.... other wise you need to write std with every single line;
+
+
+
+
+
+
+// int main() { // this is the main function we tell compiler that this is the start of the course....int here specifies the return DATA TYPE... because on successfull or unsccessfull compilation it will return 0 or 1
+
+//     cout << "So you wanna Download game? y/n"; //This is used to print the output string or anything else
+//     string response;
+//     cin >> response;
+//     if (response == "y")
+//     {
+//         cout << "click on this button to download";
+        
+//     } else if (response == "n"){
+//             cout << "Click on the <-- it will take you back home";
+//         }else{
+//             cout << "Have you decided yet or not.";
+//         }
+        
+// }
